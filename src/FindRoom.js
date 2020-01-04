9dd53d18e717ef9e7a5e575e76a3908b34d57e9e
Ross Murray <rossm@aspin.co.uk>
2020-01-04 23:26:44 +0000

    Find a room now working, display just the room name

--- src/FindRoom.js
+++ src/FindRoom.js
@@ -16,7 +16,7 @@ export default class FindRoom extends React.Component {
         super(props);
 
         this.state = {
-            rooms: ["conference-room", "meeting-room", "goldfish-bowl"],
+            rooms: ["conference-room", "meeting-room", "goldfish-bowl", "me"],
             freeRooms: [],
             room_name: ""
 
@@ -30,40 +30,49 @@ export default class FindRoom extends React.Component {
             var accessToken = await window.msal.acquireTokenSilent({
                 scopes: config.scopes
             });
-            // Get the user's events (table)
-            var freeRooms = await getFreeRooms(accessToken, moment().format('YYYY-MM-DDTHH:mm:ss'));
-            // Update the array of events in state
-            this.setState({freeRooms: freeRooms.value});
 
-            //Set the update interval of events
-            this.intervalID = setInterval(
-                () => this.updateViewport(accessToken),
-                10000
-            );
+            //Set up the free rooms array
+            let freeRoomsArray = [];
+
+            //Iterate over the rooms
+            this.state.rooms.map((room, i) => {
+                // Get the user's events (table)
+                var freeRooms =  getFreeRooms(accessToken, moment().format('YYYY-MM-DDTHH:mm:ss'), room);
+
+                //Resolve promise
+                Promise.resolve(freeRooms).then((res2) => {
+                    //Check that the events are not now
+                    if(res2.value.length === 0) {
+                        //this.props.history.push('/calendar/' + this.props.match.params.room);
+                        freeRoomsArray.push(room);
+                    }
+                });
+            });
+
+            this.setState({freeRooms: freeRoomsArray});
+
         }
         catch(err) {
             this.props.showError('ERROR', JSON.stringify(err));
         }
     }
 
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
-    }
+    getRoomName(room) {
+        var roomName = "";
+        if (room === "conference-room") {
+            roomName = "Conference Room";
+        } else  if (room === "meeting-room") {
+            roomName = "Meeting Room";
+        } else  if (room === "goldfish-bowl") {
+            roomName = "Goldfish Bowl";
+        } else {
+            return "Ross Murray";
+        }
 
-    //Update content every 10 seconds
-    updateViewport(accessToken) {
-        this.updateFreeRooms(accessToken);
-    }
-    updateFreeRooms(accessToken) {
-        getFreeRooms(accessToken, moment().format('YYYY-MM-DDTHH:mm:ss')).then(result => this.setState({
-            now: result.value
-        }))
+        return roomName;
     }
 
     render() {
-
-        const nowLength = this.state.freeRooms.length;
-        const startMeetingLink = "/calendar/" + this.props.match.params.room + "/start-meeting/";
         const backLink = "/calendar/" + this.props.match.params.room ;
         return (
             <div>
@@ -76,31 +85,14 @@ export default class FindRoom extends React.Component {
                     <Row className="section free-room">
                         <Col xs={12}><h5>Available Now</h5></Col>
 
-                        {nowLength > 0 ? (
+                        {/*Iterate over each room*/}
+                        {this.state.freeRooms.map((room, i) => {
+                            return (
                                 <>
-                                    <Col xs="8"><h2>Room Available</h2></Col>
-                                    <Col xs="4" className="text-right">
-                                        <Link to={startMeetingLink}>
-                                            <Button variant="success" size="lg">Start Meeting</Button>
-                                        </Link>
-
-                                    </Col>
-
-                                </>
-
-
-                            ) :
-                            (
-                                <>
-                                    <Col xs="8"><h2>No rooms currently available</h2></Col>
-
-
+                                    <Col xs="8" className="bottom-spacer"><h2>{this.getRoomName(room)}</h2></Col>
                                 </>
                             )
-                        }
-
-
-
+                        })}
 
                     </Row>
                 </Container>
@@ -113,16 +105,9 @@ export default class FindRoom extends React.Component {
                         </Col>
                     </Row>
                 </Container>
-
-
-
-
-
             </div>
         );
     }
-
-
 }
 
 
